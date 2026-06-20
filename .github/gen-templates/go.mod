216700b29a6c66405372bebb20d72177334103d9
kitbyte <kit8byte@gmail.com>
2026-06-20 22:39:00 +0300

    feat: add identity service events and migrate to BSR

--- .github/gen-templates/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/framecast/framecast-api-contracts/gen/go
-
-go 1.22
-
-require (
-	google.golang.org/grpc v1.64.0
-	google.golang.org/protobuf v1.34.2
-)
--- .github/gen-templates/lib.rs
+++
@@ -1,8 +0,0 @@
-// Generated package entrypoint. Do not edit files in gen/ directly.
-// Update .github/gen-templates or proto definitions, then re-run `buf generate`.
-
-pub mod video_processing {
-    pub mod v1 {
-        include!("video_processing.v1.rs");
-    }
-}
